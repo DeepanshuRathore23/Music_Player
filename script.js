8b662fb9a5f0abed273ae6fb1cd816f44e3238ed
Deepanshu Rathore <deepanshurathore2004@gmail.com>
2024-02-05 22:26:39 +0530

    change

--- script.js
+++ script.js
@@ -2,39 +2,39 @@ let player = document.getElementById("player");
 let progress = document.getElementById("progress");
 let ctrlIcon = document.getElementById("ctrl-icon");
 
-if (isFinite(player.duration)) {
-    progress.max = player.duration
-    let duration = player.duration;
-    console.log(duration);
-} else {
-    console.log("Duration is not yet available.");
+function getDetails() {
+    if (isFinite(player.duration)) {
+        progress.max = player.duration;
+        let duration = player.duration;
+        console.log(duration);
+    } else {
+        console.log("Duration is not yet available.");
+    }
 }
 
+// Wait for the loadedmetadata event before calling getDetails
+player.addEventListener("loadedmetadata", getDetails);
 
 let play = false;
 
-function playPause(){
-    if(ctrlIcon.className=="play-icon"){      
-        player.play(); 
-        play=true;
+function playPause() {
+    if (ctrlIcon.className == "play-icon") {
+        player.play();
+        play = true;
         ctrlIcon.innerHTML = `<img src=pause-icon.png  height=${40}px width=${40}px>`;
-        ctrlIcon.className="pause-icon";
-    }
-    else{        
+        ctrlIcon.className = "pause-icon";
+    } else {
         player.pause();
-        play=false;
+        play = false;
         ctrlIcon.innerHTML = `<img src=play-icon.png  height=${40}px width=${40}px>`;
-        ctrlIcon.className="play-icon";
+        ctrlIcon.className = "play-icon";
     }
 }
-// progress.max=player.duration;
 
-setInterval(()=>{
-    if(play){
-        progress.value=player.currentTime;
+setInterval(() => {
+    if (play) {
+        progress.value = player.currentTime;
     }
-},100);
+}, 100);
 
-
-ctrlIcon.addEventListener("click",playPause);
-
+ctrlIcon.addEventListener("click", playPause);
